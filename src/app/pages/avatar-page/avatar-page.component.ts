8d9a7a784d5e2cb333ed39efb023870fa75f4e42
Jaque <j.costa@dino.com.br>
2023-09-18 21:43:06 -0300

    avatar page

---
+++ src/app/pages/avatar-page/avatar-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-avatar-page',
+  templateUrl: './avatar-page.component.html',
+  styleUrls: ['./avatar-page.component.scss']
+})
+export class AvatarPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
